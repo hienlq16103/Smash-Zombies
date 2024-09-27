c16b9a291ba30d63b3047a88c502c37e105d0c3a
NaguKun <thenhanvt123@gmail.com>
2024-09-27 16:26:43 +0700

    zombie-head-logic

--- main.py
+++ main.py
@@ -2,10 +2,33 @@ import sys
 import pygame
 from pygame.locals import *
 from Scripts.spawn_data import SpawnData
-
+import random
+import time 
 # Screen resolution
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 400
+
+class ZombieHead(pygame.sprite.Sprite):
+    def __init__(self, spawn_data):
+        super().__init__()
+        self.image = pygame.image.load('img/zombie_head.png')
+        self.rect = self.image.get_rect()
+        self.kill_audio = pygame.mixer.Sound('audio/punch.wav')
+
+        self.rect.topleft = spawn_data.spawn_point()
+        self.despawn_time = time.time() + 1  # The zombie despawns after 5 seconds
+
+    def spawn(self):
+        pass
+
+    def despawn(self):
+        if time.time() > self.despawn_time:
+            self.kill()
+
+    def on_smashed(self):
+        self.kill_audio.play()
+        self.kill()  
+
 
 class App:
     """
@@ -13,62 +36,52 @@ class App:
     """
 
     def __init__(self):
-        """
-        Construct the app.
-        """
         self.__is_running = True
         self.__displaying_surface = None
         self.size = self.width, self.height = SCREEN_WIDTH, SCREEN_HEIGHT
+        self.background = None
+        self.all_sprites = pygame.sprite.Group()
+        self.spawn_data = SpawnData()
 
     def on_init(self):
-        """
-        Init pygame and create gameplay window.
-        """
         pygame.init()
         self.__displaying_surface = pygame.display.set_mode(self.size)
-        
-        # Tải hình nền và thay đổi kích thước cho vừa cửa sổ
-        self.__background = pygame.image.load('img/background.jpg').convert()
-        self.__background = pygame.transform.scale(self.__background, (self.width, self.height))
-        
+        self.background = pygame.image.load('img/background.jpg')
+        self.background = pygame.transform.scale(self.background, (self.width, self.height))
+        pygame.mixer.init()
+
+        # Create and spawn a zombie head
+        self.spawn_zombie()
+
         return self.__displaying_surface
 
-    def on_event(self, event):
-        """
-        Handle system event.
+    def spawn_zombie(self):
+        zombie = ZombieHead(self.spawn_data)
+        self.all_sprites.add(zombie)
 
-        :param event Event: Event to handle
-        """
+    def on_event(self, event):
         if event.type == pygame.QUIT:
             self.__is_running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            for sprite in self.all_sprites:
+                if sprite.rect.collidepoint(event.pos):
+                    sprite.on_smashed()
 
     def on_loop(self):
-        """
-        Updating the game world each frame.
-        """
-        pass
+        for sprite in self.all_sprites:
+            sprite.despawn()  # Check if zombies need to despawn
+        self.all_sprites.update()
 
     def on_render(self):
-        """
-        Rendering the game each frame.
-        """
-        # Vẽ hình nền lên màn hình
-        self.__displaying_surface.blit(self.__background, (0, 0))
-
-        # Cập nhật màn hình
+        self.__displaying_surface.blit(self.background, (0, 0))
+        self.all_sprites.draw(self.__displaying_surface)
         pygame.display.flip()
 
     def on_cleanup(self):
-        """
-        Clean up the app after being closed.
-        """
         pygame.quit()
         sys.exit()
 
     def on_execute(self):
-        """
-        Handle the gameplay loop.
-        """
         if self.on_init() is None:
             self.__is_running = False
 
